f7b27b3ffc8a7dde2a316dc7f72e726bbded6ae5
sidestepism <fushimi.ryohe.i@gmail.com>
2014-07-01 16:32:25 +0900

    Initial Commit

---
+++ pirafu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pirafu
+//
+//  Created by Ryohei Fushimi on 7/1/14.
+//  Copyright (c) 2014 dsj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
